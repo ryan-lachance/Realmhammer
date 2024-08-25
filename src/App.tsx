97a3dfbebb49b7a388b6590b400cc638e00645a4
ryan-lachance <r-lachance@hotmail.com>
2024-08-24 21:12:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Button from '@mui/material/Button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button variant='contained'>Test</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
